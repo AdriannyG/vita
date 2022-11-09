29b495dafe8f527896747810b82c959d61ed4463
AdriannyG <117771551+AdriannyG@users.noreply.github.com>
2022-11-09 13:49:56 -0400

    Viita

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculo</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link href="https://fonts.googleapis.com/css2? family=Langosta&familia=Roboto:wght@100&family=Source+Sans+Pro:wght@200;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="name">
+        <div class="img">
+            <img src="fotoperfil.jpg" alt="">
+        </div>
+        <div class="content">
+            <p class="p1">Adrianny Gabriela Orta Jiménez</p>
+        </div>
+    </div>
+    <div class="info">
+        <p class="titulosecc">Datos Personales</p>
+        <p class="tituloinfo">Nombres y Apellidos:</p>
+        <p class="infoInfo">Adrianny Gabriela Orta Jiménez</p><br>
+        <p class="tituloinfo">Cedula de Identidad:</p>
+        <p class="infoInfo">29.866.741</p><br>
+        <p class="tituloinfo">Estado Civil:</p>
+        <p class="infoInfo">Soltera</p><br>
+        <p class="tituloinfo">Fecha y Lugar de Nacimiento:</p>
+        <p class="infoInfo"> 18.04.2002, Maracay. Edo: Aragua</p><br>
+        <p class="tituloinfo">Direccion:</p>
+        <p class="infoInfo">Tocorón, Municipio Augusto Mijares. Edo Aragua</p><br>
+        <p class="tituloinfo">Teléfono:</p>
+        <p class="infoInfo">0412-8480697</p><br>
+        <p class="tituloinfo">Correo Electronico:</p>
+        <p class="infoInfo">adriannygabriela2015@gmail.com</p><br>
+    </div>
+
+    <div class="info">
+        <p class="titulosecc">Estudios Realizados</p>
+        <p class="tituloinfo">Educación Básica:</p>
+        <p class="infoInfo">U.E.E ''Juan Bautista Plaza'' - 6to Grado.</p><br>
+        <p class="tituloinfo">Educación Diversificada:</p>
+        <p class="infoInfo">U.E.N ''José Rafael Núñez Tenorio'' - Bachiller en Ciencias.</p><br>
+    </div>
+
+    <div class="info">
+        <p class="titulosecc">Habilidades y Destrezas</p>
+        <p class="tituloinfo">Actitud Positiva.</p><br>
+        <p class="tituloinfo">Dispuesta a aprender nuevos conocimientos.</p>
+    </div>
+</body>
+</html>
